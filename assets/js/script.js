d79318da41f4cb21c70e82d1d27ab2fba4f5e562
JackLamb99 <jacklamb15@yahoo.co.uk>
2024-04-05 19:23:37 +0100

    Add function for displaying the reveal card upon clicking either action button

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,15 @@ var playBtn = document.getElementById("btn-play");
 // Gets the back button to return to the Menu page
 var returnBtn = document.getElementById("back-arrow");
 
+// Gets the "guess" card
+var guessCard = document.getElementById("guess-card");
+
+// Gets the "reveal" card
+var revealCard = document.getElementById("reveal-card");
+
+// Gets the action buttons to display the "reveal" card
+var actionBtns = document.getElementsByClassName("btn-reveal-card");
+
 // When the user clicks the "Instructions" button, open the Instructions page
 instructionsBtn.onclick = function () {
   instructions.style.display = "block";
@@ -35,3 +44,11 @@ playBtn.onclick = function () {
   game.style.display = "block";
   menu.style.display = "none";
 };
+
+// When the user clicks either action button, display the "reveal" card
+for (var i = 0; i < actionBtns.length; i++) {
+  actionBtns[i].addEventListener("click", function () {
+    revealCard.style.display = "block";
+    guessCard.style.display = "none";
+  });
+}
